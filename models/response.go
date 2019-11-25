cbf1e1447baf9002ef481f70a7854b207d8f4a28
Manfred Kogler <manfred.kogler@gmail.com>
2019-11-25 14:47:15 +0100

    represent JSON responses as cascaded structs

--- models/response.go
+++ models/response.go
@@ -2,49 +2,31 @@ package models
 
 // CalculateRouteResponse embodies the interesting parts of the calculateroute request
 type CalculateRouteResponse struct {
-	Response cRresponse `json:"response"`
-}
-
-type cRresponse struct {
-	Route []route `json:"route"`
-}
-
-type route struct {
-	Summary summary `json:"summary"`
-}
-
-type summary struct {
-	Distance   int `json:"distance"`
-	TravelTime int `json:"travelTime"`
+	Response struct {
+		Route []struct {
+			Summary struct {
+				Distance   int `json:"distance"`
+				TravelTime int `json:"travelTime"`
+			} `json:"summary"`
+		} `json:"route"`
+	} `json:"response"`
 }
 
 // ForwardGeocoderResponse embodies the interesting parts of the forward geocode request
 type ForwardGeocoderResponse struct {
-	Response fGCresponse `json:"Response"`
-}
-
-type fGCresponse struct {
-	View []view `json:"View"`
-}
-
-type view struct {
-	Result []result `json:"Result"`
-}
-
-type result struct {
-	Location location `json:"Location"`
-}
-
-type location struct {
-	NavigationPosition []navigationPosition `json:"NavigationPosition"`
-	Address            address              `json:"Address"`
-}
-
-type navigationPosition struct {
-	Latitude  float64 `json:"Latitude"`
-	Longitude float64 `json:"Longitude"`
-}
-
-type address struct {
-	Label string `json:"Label"`
+	Response struct {
+		View []struct {
+			Result []struct {
+				Location struct {
+					NavigationPosition []struct {
+						Latitude  float64 `json:"Latitude"`
+						Longitude float64 `json:"Longitude"`
+					} `json:"NavigationPosition"`
+					Address struct {
+						Label string `json:"Label"`
+					} `json:"Address"`
+				} `json:"Location"`
+			} `json:"Result"`
+		} `json:"View"`
+	} `json:"Response"`
 }
