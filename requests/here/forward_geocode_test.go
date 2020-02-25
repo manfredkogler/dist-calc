bea4742d5ea4d3c9c8533f325f4ceafc77ecf564
Manfred Kogler <manfred.kogler@gmail.com>
2020-02-25 17:42:16 +0100

    properly implemented tests

--- requests/here/forward_geocode_test.go
+++ requests/here/forward_geocode_test.go
@@ -8,29 +8,56 @@ import (
 
 // test data BEGIN
 
-// Locations test data
-var Locations []models.Loc = []models.Loc{
-	{Addr: "Lambertgasse 4", Lat: "48.211836", Lng: "16.319760"},
-	{Addr: "Schottenring 1", Lat: "48.215239", Lng: "16.365305"},
+// Test data
+type testData struct {
+	addr string
+	loc  models.Loc
 }
 
-const (
-	// Lambertgasse test data
-	Lambertgasse = 0
-	// Schottenring test data
-	Schottenring = 1
-)
+var testDatas []testData = []testData{
+	{
+		"Lambertgasse 4 Wien",
+		models.Loc{
+			Addr: "Lambertgasse 4, 1160 Wien, Österreich",
+			Lat:  "48.211820",
+			Lng:  "16.319660"},
+	},
+	{
+		"Schottenring 1 Wien",
+		models.Loc{
+			Addr: "Schottenring 1, 1010 Wien, Österreich",
+			Lat:  "48.214180",
+			Lng:  "16.362830"},
+	},
+}
 
 // test data END
 
 func TestForwardGeocode(t *testing.T) {
-	hereService := HereGeoQuery{}
+	geoService := HereGeoQuery{}
 
-	routeInfo := hereService.CalculateRoute(Locations[Lambertgasse], Locations[Schottenring])
-	routeInfo = hereService.CalculateRoute(Locations[Schottenring], Locations[Lambertgasse])
-	_ = routeInfo
+	for _, td := range testDatas {
+		checkForwardGeocode(t, geoService, td)
+	}
+}
 
-	location := hereService.ForwardGeocode("Schottenring 1 Wien")
-	location = hereService.ForwardGeocode("Lambertgasse 4 Wien")
-	_ = location
+func checkForwardGeocode(t *testing.T, s HereGeoQuery, td testData) {
+	got := s.ForwardGeocode(td.addr)
+	if got != td.loc {
+		t.Errorf("ForwardGeocode() = %v; want %v", got, td.loc)
+	}
+}
+
+func TestCalculateRoute(t *testing.T) {
+	geoService := HereGeoQuery{}
+
+	want := models.RouteInfo{
+		Distance:   3736,
+		TravelTime: 583,
+	}
+
+	got := geoService.CalculateRoute(testDatas[0].loc, testDatas[1].loc)
+	if got != want {
+		t.Errorf("CalculateRoute() = %v; want %v", got, want)
+	}
 }
